c20f531c3cf702248ea934ac401153f6c7638dd1
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2023-07-19 10:18:05 +0530

    video 45 complited

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,13 @@ const express=require('express')
 connectToMongo();
 const app=express()
 const port=3000;
-app.get('/',(req,res)=>{
-    res.send('hello world')
-})
+//middleWare to use request.body to put a requent on json in thunder client.
+//content type application json.
+app.use(express.json())
+//endpoints:app.get and other
+//Available Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 app.listen(port,()=>{
-    console.log(`example app listening at https://localhost:${port}`)
+    console.log(`example app listening at http://localhost:${port}`)
 })
