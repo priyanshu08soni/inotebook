24514cc91f0caf2aa8eec786fa24bd56d6ecb087
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2023-08-11 13:18:38 +0530

    video 68 completed

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const connectToMongo=require('./db');
 const express=require('express');
+var cors=require('cors')
 connectToMongo();
 const app=express();
 const port=5000;
+app.use(cors())
+app.use(express.json())
 //middleWare to use request.body to put a requent on json in thunder client.
 //content type application json.
 app.use(express.json())
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,66 +4,89 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState =(props)=>{
-    const notesInitial=[
-        {
-          "_id": "64bdeece22e0d220bfed393a",
-          "user": "64bb7918f11c2b478f6a56b0",
-          "title": "My title",
-          "description": "Please wake up early",
-          "tag": "presonal",
-          "date": "2023-07-24T03:23:58.687Z",
-          "__v": 0
-        },
-        {
-          "_id": "64bdeece22e0d220bfed393a",
-          "user": "64bb7918f11c2b478f6a56b0",
-          "title": "My title",
-          "description": "Please wake up early",
-          "tag": "presonal",
-          "date": "2023-07-24T03:23:58.687Z",
-          "__v": 0
-        },
-        {
-          "_id": "64bdeece22e0d220bfed393a",
-          "user": "64bb7918f11c2b478f6a56b0",
-          "title": "My title",
-          "description": "Please wake up early",
-          "tag": "presonal",
-          "date": "2023-07-24T03:23:58.687Z",
-          "__v": 0
-        },
-        {
-          "_id": "64bdeece22e0d220bfed393a",
-          "user": "64bb7918f11c2b478f6a56b0",
-          "title": "My title",
-          "description": "Please wake up early",
-          "tag": "presonal",
-          "date": "2023-07-24T03:23:58.687Z",
-          "__v": 0
-        },
-        {
-            "_id": "64bdeece22e0d220bfed393a",
-            "user": "64bb7918f11c2b478f6a56b0",
-            "title": "My title",
-            "description": "Please wake up early",
-            "tag": "presonal",
-            "date": "2023-07-24T03:23:58.687Z",
-            "__v": 0
-        },
-        {
-            "_id": "64bdeece22e0d220bfed393a",
-            "user": "64bb7918f11c2b478f6a56b0",
-            "title": "My title",
-            "description": "Please wake up early",
-            "tag": "presonal",
-            "date": "2023-07-24T03:23:58.687Z",
-            "__v": 0
-        }
-      ]
+  const host="http://localhost:5000";
+    const notesInitial=[]
       const [notes,setNotes]=useState(notesInitial)
+      //GET all Notes
+      const getNotes=async(title,description,tag)=>{
+        //Api Call
+        const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+          method: "GET", 
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRiYjc5MThmMTFjMmI0NzhmNmE1NmIwIn0sImlhdCI6MTY5MDAwODY4NX0.9tYQcs0WXawoE1De43XBw7dVAIEX7L_J7ad5y1XrD-Q",
+
+          },
+        });
+        const json=await response.json()
+        console.log(json)
+        setNotes(json);
+      }
+      const addNote=async(title,description,tag)=>{
+        //Api Call
+        const response = await fetch(`${host}/api/notes/addnote`, {
+          method: "POST", 
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRiYjc5MThmMTFjMmI0NzhmNmE1NmIwIn0sImlhdCI6MTY5MDAwODY4NX0.9tYQcs0WXawoE1De43XBw7dVAIEX7L_J7ad5y1XrD-Q",
+
+          },
+          body: JSON.stringify({title,description,tag}),
+        });
+        const note= await response.json(); 
+        setNotes(notes.concat(note))
+      }
+      //Delete a Note
+      const deleteNote=async(id)=>{
+        //Api Call
+        const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+          method: "DELETE", 
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRiYjc5MThmMTFjMmI0NzhmNmE1NmIwIn0sImlhdCI6MTY5MDAwODY4NX0.9tYQcs0WXawoE1De43XBw7dVAIEX7L_J7ad5y1XrD-Q",
+
+          },
+        });
+        const json= await response.json(); 
+        console.log(json)
+        console.log("delete "+id)
+        //if both id's are same then it will remain otherwise it will discarded.
+        const newNotes=notes.filter((note)=>{return note._id!==id})
+        setNotes(newNotes)
+
+      }
+      //Edit a Note
+      const editNote=async(id,title,description,tag)=>{
+        //API call.
+          const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+            method: "PUT", 
+            headers: {
+              "Content-Type": "application/json",
+              "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRiYjc5MThmMTFjMmI0NzhmNmE1NmIwIn0sImlhdCI6MTY5MDAwODY4NX0.9tYQcs0WXawoE1De43XBw7dVAIEX7L_J7ad5y1XrD-Q",
+
+            },
+            body: JSON.stringify({title,description,tag}),
+          });
+          const json= await response.json(); 
+          console.log(json);
+
+        let newNotes=JSON.parse(JSON.stringify(notes));
+        //logic to edit in client.
+        for (let index = 0; index < newNotes.length; index++) {
+          const element = newNotes[index];
+          if(element._id===id){
+            newNotes[index].title=title;
+            newNotes[index].description=description;
+            newNotes[index].tag=tag;
+            break;
+          }
+        }  
+        setNotes(newNotes);
+        }
+      
     return(
 
-        <NoteContext.Provider value={{notes,setNotes}}>
+        <NoteContext.Provider value={{notes,addNote,deleteNote,editNote,getNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
