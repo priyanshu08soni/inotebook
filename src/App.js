de820c7d8a1731e16f2093c15b030c45e2eebb84
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2023-08-11 17:06:15 +0530

    fdfdfdfd

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Home } from './components/Home';
 import { About } from './components/About';
 import NoteState from './context/notes/NoteState';
 import { Alert } from './components/Alert';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 function App() {
   return (
     <>
@@ -17,6 +19,8 @@ function App() {
               <Routes>
                 <Route exact path="/" element={<Home/>}></Route>
                 <Route exact path="/about" element={<About/>}></Route>
+                <Route exact path="/login" element={<Login/>}></Route>
+                <Route exact path="/signup" element={<SignUp/>}></Route>
               </Routes>
             </div>
         </Router>
---
+++ src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+      I am Signup.
+    </div>
+  )
+}
+
+export default SignUp
